85b796ddd359498ad72d750fb823d21986c0d6b8
PedroLucas-inf2am18 <65040183+PedroLucas-inf2am18@users.noreply.github.com>
2020-05-11 11:17:49 -0300

    Create TvSerie.java

---
+++ TvSerie.java
@@ -0,0 +1,95 @@
+
+import androidx.annotation.NonNull;
+
+import androidx.annotation.Nullable;
+
+import androidx.room.ColumnInfo;
+
+import androidx.room.Entity;
+
+import androidx.room.PrimaryKey;
+
+
+@Entity(tableName = "TVSeries")
+
+
+public class TvSerie {
+    
+
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "SerieId")
+    private int mId;
+
+    @Nullable
+    @ColumnInfo(name = "SerieTitle")
+    private String mTitle;
+
+    private String mPersonage;
+
+    
+    public TvSerie(@Nullable String title, String personage, int score, String creationDate) {
+
+        mTitle = title;
+        mPersonage = personage;
+        mScore = score;
+        mCreationDate = creationDate;
+
+    
+    }
+
+
+
+    public int getId() {
+        return mId;
+    }
+
+    public void setId(int id) {
+        mId = id;
+    }
+
+
+    public String getTitle() {
+        return mTitle;
+
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+
+    public String getPersonage() {
+        return mPersonage;
+    }
+
+    public void setPersonage(String personage) {
+        mPersonage = personage;
+    }
+
+     
+    public int getScore() {
+        return mScore;
+    }
+
+    public void setScore(int score) {
+        mScore = score;
+    }
+
+    
+    public String getCreationDate() {
+        return mCreationDate;
+    }
+
+    public void setCreationDate(String creationDate) {
+        mCreationDate = creationDate;
+    }
+ 
+
+    private int mScore;
+
+    private String mCreationDate;
+
+
+
+}
